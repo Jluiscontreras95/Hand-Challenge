89e14846461a0f49bf539bcce4fdad634ccc75b1
Jorge Contreras <jluiscontreras95@gmail.com>
2023-04-01 18:50:18 -0400

    solución del challenge

---
+++ test.js
@@ -0,0 +1,14 @@
+/* global test, expect */
+const translate = require('./index.js')
+
+test('it translates "Hello" accordingly', () => {
+    expect(
+        translate('👇🤜👇👇👇👇👇👇👇👉👆👈🤛👉👇👊👇🤜👇👉👆👆👆👆👆👈🤛👉👆👆👊👆👆👆👆👆👆👆👊👊👆👆👆👊')
+    ).toBe('Hello');
+})
+
+test.skip('it translates "Hello Word!" accordingly', () => {
+    expect(
+        translate('👉👆👆👆👆👆👆👆👆🤜👇👈👆👆👆👆👆👆👆👆👆👉🤛👈👊👉👉👆👉👇🤜👆🤛👆👆👉👆👆👉👆👆👆🤜👉🤜👇👉👆👆👆👈👈👆👆👆👉🤛👈👈🤛👉👇👇👇👇👇👊👉👇👉👆👆👆👊👊👆👆👆👊👉👇👊👈👈👆🤜👉🤜👆👉👆🤛👉👉🤛👈👇👇👇👇👇👇👇👇👇👇👇👇👇👇👊👉👉👊👆👆👆👊👇👇👇👇👇👇👊👇👇👇👇👇👇👇👇👊👉👆👊👉👆👊')
+    ).toBe('Hello Word!');
+})
